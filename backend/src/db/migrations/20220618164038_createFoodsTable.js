8f91102642155ef454c4de669b6e5d156a92319d
Zhi Qiang Zheng <zhizheng33782@gmail.com>
2022-07-23 02:25:36 -0400

    Update 20220618164038_createFoodsTable.js
    change amount to specifictype, text ARRAY

--- backend/src/db/migrations/20220618164038_createFoodsTable.js
+++ backend/src/db/migrations/20220618164038_createFoodsTable.js
@@ -10,7 +10,7 @@ exports.up = function (knex) {
     table.string('category').notNullable();
     table.boolean('spicy').defaultTo(0);
     table.boolean('available').defaultTo(1);
-    table.integer('amount').nullable();
+    table.specificType('amount', 'text ARRAY').nullable();
     table.specificType('options', 'text ARRAY').nullable();
     table.specificType('size', 'text ARRAY').nullable();
     table.timestamps(true, true);
