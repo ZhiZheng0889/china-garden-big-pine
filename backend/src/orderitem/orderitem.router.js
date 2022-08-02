7ba99fcc51842a98f9a2f1eeecdb2867935d1ac9
ZhiZheng0889 <zhizheng33782@gmail.com>
2022-08-02 00:41:10 -0400

    creating apis for shopping cart

--- backend/src/orderitem/orderitem.router.js
+++ backend/src/orderitem/orderitem.router.js
@@ -4,4 +4,16 @@ const methodNotAllowed = require('../errors/methodNotAllowed');
 
 router.route('/').get(controller.list).all(methodNotAllowed);
 
+app.post('/cart/update', (req, res) => {
+    let ids = req.body["product_id[]"];
+    let qtys = req.body["qty[]"];
+    if(Security.isValidNonce(req.body.nonce, req)) {
+        Cart.updateCart(ids, qtys);
+        Cart.saveCart(req);
+        res.redirect('/cart');
+    } else {
+        res.redirect('/');
+    }
+    });
+
 module.exports = router;
--- backend/src/orderitem/orderitem.service.js
+++ backend/src/orderitem/orderitem.service.js
@@ -1,16 +1,13 @@
 const knex = require('../db/connection');
 
-const TABLE = 'foods';
-
-function list() {
-  return knex(TABLE).select('*');
-}
-
-function listByCategory(category) {
-  return knex(TABLE).select('*').where({ category });
-}
-
-module.exports = {
-  list,
-  listByCategory,
+class Cart {
+  constructor() {
+     this.data = {};
+     this.data.items = [];
+     this.data.totals = 0;
+     this.data.formattedTotals = '';
+  }
 };
+
+module.exports = new Cart();
+
