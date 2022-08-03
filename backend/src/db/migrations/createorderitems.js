2d43d7a9ba9118baf21a59797b0b6d32bc219e18
ZhiZheng0889 <zhizheng33782@gmail.com>
2022-08-02 21:22:29 -0400

    table adjustment

--- backend/src/db/migrations/createorderitems.js
+++ backend/src/db/migrations/createorderitems.js
@@ -5,12 +5,15 @@
  exports.up = function (knex) {
     return knex.schema.createTable('OrderItems', (table) => {
       table.increments('OrderItems_Id').primary().notNullable();
-      table.string('FoodID').notNullable();
-      table.string('name').notNullable();
-      table.money('price').notNullable();
+      table.integer('user_id').index().references('user_id').inTable('users');
+      table.string('food_id').notNullable().index().references('user_id').inTable('users');
+      table.string('name').notNullable().index().references('food_id').inTable('foods');
+      table.money('price').notNullable().index().references('price').inTable('foods');
       table.numeric('quantity').notNullable();
-
-      table.timestamps(true, true);
+      table.string('status');
+      table.timestamp('created_at').defaultTo(knex.raw('now()'));
+      table.timestamp('updated_at').nullable();
+      table.timestamp('deleted').nullable();
     });
   };
   
