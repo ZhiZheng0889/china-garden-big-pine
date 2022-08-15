5474f8e3f7083ccfda49bb0bb766f5233f4a288e
ZhiZheng0889 <zhizheng33782@gmail.com>
2022-08-14 23:32:00 -0400

    test

--- backend/src/db/migrations/20220806023604_createOrderItemsTable.js
+++ backend/src/db/migrations/20220806023604_createOrderItemsTable.js
@@ -4,7 +4,11 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('order_items', (table) => {
-    table.increments('order_item_id').primary().notNullable();
+    table.integer('order_id').notNullable().references('order_id').inTable('orders');
+    table.integer('food_id').notNullable().references('food_id').inTable('foods');
+    table.string('requests', 500).Nullable();
+
+
   });
 };
 
