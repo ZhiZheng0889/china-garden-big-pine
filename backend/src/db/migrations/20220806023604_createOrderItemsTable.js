f39437ca3faa3d3b82f4f7d79e691bad378dfb03
admclamb <adylanmclamb@yahoo.com>
2022-08-14 23:39:15 -0400

    fix nullable

--- backend/src/db/migrations/20220806023604_createOrderItemsTable.js
+++ backend/src/db/migrations/20220806023604_createOrderItemsTable.js
@@ -4,11 +4,17 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('order_items', (table) => {
-    table.integer('order_id').notNullable().references('order_id').inTable('orders');
-    table.integer('food_id').notNullable().references('food_id').inTable('foods');
-    table.string('requests', 500).Nullable();
-
-
+    table
+      .integer('order_id')
+      .notNullable()
+      .references('order_id')
+      .inTable('orders');
+    table
+      .integer('food_id')
+      .notNullable()
+      .references('food_id')
+      .inTable('foods');
+    table.string('requests', 500).nullable();
   });
 };
 
