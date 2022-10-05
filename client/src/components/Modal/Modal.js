2f93a03dd6a495fa44145bfb96f114523376f3e7
admclamb <adylanmclamb@yahoo.com>
2022-10-04 23:03:31 -0400

    adding modal

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -25,41 +25,67 @@ const Modal = ({ food, setCart, cart, setFood }) => {
     ]);
   };
   return (
-    <div
+    <article
+      className={styles.modal}
       id="foodModal"
-      tabindex="-1"
+      tabIndex="-1"
       aria-labelledby="foodModalLabel"
-      aria-hidden="true"
+      aria-hidden={food ? true : false}
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="foodModalLabel">
-              Modal title
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <header className={styles.header}>
+        <h4>{name}</h4>
+        <button
+          type="button"
+          className={`btn`}
+          data-bs-dismiss="modal"
+          aria-label="Close"
+          onClick={() => setFood(null)}
+        >
+          <i class="fa-regular fa-xmark"></i>
+        </button>
+      </header>
+      <section className={styles.main}>
+        <p>Woohoo, you're reading this text in a modal!</p>
+      </section>
+      <footer className={styles.footer}>
+        <button className="btn btn-primary">Add to Cart</button>
+      </footer>
+    </article>
+    // <div
+    //   id="foodModal"
+    //   tabindex="-1"
+    //   aria-labelledby="foodModalLabel"
+    //   aria-hidden="true"
+    // >
+    //   <div class="modal-dialog">
+    //     <div class="modal-content">
+    //       <div class="modal-header">
+    //         <h5 class="modal-title" id="foodModalLabel">
+    //           Modal title
+    //         </h5>
+    //         <button
+    //           type="button"
+    //           class="btn-close"
+    //           data-bs-dismiss="modal"
+    //           aria-label="Close"
+    //         ></button>
+    //       </div>
+    //       <div class="modal-body">...</div>
+    //       <div class="modal-footer">
+    //         <button
+    //           type="button"
+    //           class="btn btn-secondary"
+    //           data-bs-dismiss="modal"
+    //         >
+    //           Close
+    //         </button>
+    //         <button type="button" class="btn btn-primary">
+    //           Save changes
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
