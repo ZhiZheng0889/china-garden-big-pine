cc544fc58704252bd3ba4a517584c99221032e2c
admclamb <adylanmclamb@yahoo.com>
2022-10-21 14:17:54 -0400

    adding context for cart

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import PageRoutes from './pages/Routes';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import { CartContext } from './context/CartContext';
 function App() {
   const [user, setUser] = useState(null);
+  const [cart, setCart] = useState([]);
   return (
     <>
-      <header>
-        <Navbar user={user} />
-      </header>
-      <PageRoutes />
+      <CartContext.Provider value={cart}>
+        <header>
+          <Navbar user={user} />
+        </header>
+        <PageRoutes />
+      </CartContext.Provider>
     </>
   );
 }
