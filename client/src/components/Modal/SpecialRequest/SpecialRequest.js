1597b9ec13694bf20246caa354607f853f9e59da
admclamb <adylanmclamb@yahoo.com>
2022-10-21 16:41:43 -0400

    adding more options and sizes

--- client/src/components/Modal/SpecialRequest/SpecialRequest.js
+++ client/src/components/Modal/SpecialRequest/SpecialRequest.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import styles from './SpecialRequest.module.css';
-const SpecialRequest = ({specialRequest, setSpecialRequest}) => {
+const SpecialRequest = ({ specialRequest, setSpecialRequest }) => {
   // create ability to fill out special requests
-  function handler(event){
-    const {value}= event.target;
-    setSpecialRequest(value);
+  function handler(event) {
+    const { value } = event.target;
+    if (specialRequest.length <= 299) {
+      setSpecialRequest(value);
+    }
   }
-  ;
   return (
-    <div className="mt20">
-      <label htmlFor="item_special_instructions" id="special_instructions_label">
-        Special instructions :
-      </label>
-      <p className="description">
-        NOTE EXTRA CHARGES MAY BE INCURRED FOR ADDITIONS IN THIS SECTION. 
-        PLEASE INCLUDE THE AMOUNT BY $ FOR ADDING MORE INGREDIENT TO A DISH.
-      </p>
-      <form id='form' method='post'>
-      <textarea
-        value={specialRequest}
-        onChange={handler}
-        className="mb10"
-        rows={10}
-        cols={60}
-        name="param_special~instructions_s_n_500"
-        id="item_special_instructions"
-        maxLength="500"
-      ></textarea>
+    <>
+      <h3 className="modal-header">Special Request</h3>
+      <form className={styles.form}>
+        <p className={styles.description}>
+          **NOTE EXTRA CHARGES MAY BE INCURRED FOR ADDITIONS IN THIS SECTION.
+          PLEASE INCLUDE THE AMOUNT BY $ FOR ADDING MORE INGREDIENT TO A DISH.**
+        </p>
+        <textarea
+          value={specialRequest}
+          onChange={handler}
+          className={styles.textArea}
+          id="specialRequest"
+          name="specialRequest"
+        ></textarea>
       </form>
-    </div>
+    </>
   );
 };
 
