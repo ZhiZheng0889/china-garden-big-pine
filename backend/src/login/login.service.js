9d05b1d871ae9ace824578df5fd5e97db0bace76
ZhiZheng0889 <zhizheng33782@gmail.com>
2022-11-18 22:08:35 -0500

    test

--- backend/src/login/login.service.js
+++
@@ -1,3 +0,0 @@
-const knex = require('../db/connection');
-
-module.exports = {};
--- backend/src/register/register.router.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const controller = require('./register.controller');
-const methodNotAllowed = require('../errors/methodNotAllowed');
-router.route('/').post(controller.create).all(methodNotAllowed);
-
-module.exports = router;
