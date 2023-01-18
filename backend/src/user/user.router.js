57f6960c2df8a6a981db90ff47bd80d4018ecca7
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-01-17 22:05:23 -0500

    test

--- backend/src/user/user.router.js
+++ backend/src/user/user.router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('./user.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
+const passport = require('passport');
 
 router
   .route('/login/token')
@@ -9,7 +10,7 @@ router
 router.route('/login').post(controller.login).all(methodNotAllowed);
 router.route('/logout').post(controller.logout).all(methodNotAllowed);
 router.route('/:user_id').get(controller.getUserById).all(methodNotAllowed);
-router.get( router, passport.authenticate('2fa', { session: false }), dashboardController.handleDashboard);
+//router.get( router, passport.authenticate('2fa', { session: false }), dashboardController.handleDashboard);
 router
   .route('/')
   .get(controller.getUsers)
