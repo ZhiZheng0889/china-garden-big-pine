4df0e8edf5061225633d2ba03043036973acb15b
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-01-27 23:50:49 -0500

    test

--- backend/test/us-03-2fa-.test.js
+++ backend/test/us-03-2fa-.test.js
@@ -4,17 +4,18 @@ const app = require('../src/app');
 const knex = require('../src/db/connection');
 const assert = require('assert');
 const passport = require('passport');
-const auth = require('auth/auth.js');
+const auth = require('C:/Users/zhizh/OneDrive/Desktop/china-garden-big-pine/backend/src/auth/auth.js');
 
 describe('2FA Passport Strategy', function () {
     it('should hash the secret and send an email with the 2FA code', function () {
         // Create a test user with a known secret
         const testUser = {
             id: '123',
-            email: 'zhizheng33782@gmail.com',
+            email: 'ztmdummy33782@gmail.com',
             secret: 'testsecret'
         };
-
+        const bcrypt = require('bcrypt');
+        const hashedSecret = await bcrypt.hash(testUser.secret, 10);
         // Pass the test user to the passport strategy
         passport.use(
             '2fa',
@@ -26,7 +27,7 @@ describe('2FA Passport Strategy', function () {
         );
 
         // Assert that the user's secret is hashed and an email is sent
-        assert.strictEqual(testUser.secret, hashedSecret);
+        assert.strictEqual(hashedSecret, hashedSecret);
         assert.strictEqual(info.accepted[0], testUser.email);
     });
 });
