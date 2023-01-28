6f898e834d95f0b55a47f50b8b5aa0ee14c46042
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-01-27 23:43:34 -0500

    test

--- backend/test/us-03-2fa-.test.js
+++ backend/test/us-03-2fa-.test.js
@@ -4,23 +4,24 @@ const app = require('../src/app');
 const knex = require('../src/db/connection');
 const assert = require('assert');
 const passport = require('passport');
+const auth = require('auth\auth.js');
 
 describe('2FA Passport Strategy', function () {
     it('should hash the secret and send an email with the 2FA code', function () {
         // Create a test user with a known secret
         const testUser = {
             id: '123',
-            email: 'ztmdummy33782@gmail.com',
+            email: 'zhizheng33782@gmail.com',
             secret: 'testsecret'
         };
-s
+
         // Pass the test user to the passport strategy
         passport.use(
             '2fa',
             new passport.Strategy(async function (req, done) {
                 req.user = testUser;
                 // Call the passport strategy
-                await require('path/to/script')(req, done);
+                await auth.twoFactorAuth(req, done);
             })
         );
 
