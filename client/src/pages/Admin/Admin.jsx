b3e370b719cbe2953c81b1c45c6c84c61088e6be
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-02-17 02:05:26 -0500

    test

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,7 +1,63 @@
-const Admin = ({ user }) => {
+/* const Admin = ({ user }) => {
   if (!user && !user.isAdmin) {
   }
   return null;
 };
+
+export default Admin;
+*/
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@material-ui/core';
+
+function Admin() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+    }
+    fetchProducts();
+  }, []);
+
+  async function handleDelete(id) {
+    await axios.delete(`/api/products/${id}`);
+    setProducts(products.filter((product) => product.id !== id));
+  }
+
+  return (
+    <div>
+      <h1>E-Commerce Admin</h1>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>
+                  <Button variant="contained" color="secondary" onClick={() => handleDelete(product.id)}>
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
 
 export default Admin;
