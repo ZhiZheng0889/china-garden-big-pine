2b71e44cb8eb2cdfd4644c1ec2ab0f2ecc9a4f2e
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-02-27 21:20:33 -0500

    test

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -11,12 +11,6 @@ import Footer from '../../components/Footer/Footer';
 import ProfileFavoriteMeals from '../../components/Profile/ProfileFavoriteMeals/ProfileFavoriteMeals';
 import ProfileFavoriteOrders from '../../components/Profile/ProfileFavoriteOrders/ProfileFavoriteOrders';
 import ProfileOrders from '../../components/Profile/ProfileOrders/ProfileOrders';
-import ErrorAlert from '../../errors/ErrorAlert';
-import { isObjectEmpty } from '../../utils/isObjectEmpty';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import ErrorAlert from '../../errors/ErrorAlert';
-import Card from '../../components/Card/Card';
 
 const Admin = ({ user }) => {
   const [error, setError] = useState(null);
