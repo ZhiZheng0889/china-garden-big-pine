faef7936e581e3f1c59791f2c5217023320bbeaf
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-02-28 23:29:09 -0500

    test

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,45 +1,24 @@
-//admin.jsx for admin page
-//admin page will have a table of all users
-//admin page will have a table of all products
-//admin page will have a table of all categories
+//admin page
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './Admin.css';
 
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Card from '../../components/Card/Card';
-import Footer from '../../components/Footer/Footer';
-import {isObjectEmpty} from '../../utils/isObjectEmpty';
+//change the price for food items
+//change the price for food options
 
-const Admin = ({ user }) => {
-  const [error] = useState(null);
-  const navigate = useNavigate();
-  if (isObjectEmpty(user)) {
-    navigate('/');
-  }
+const Admin = () => {
   return (
-    <main className="min-h-screen bg-slate-100 flex justify-center pt-6">
-      <div className="container grid grid-cols-1 md:grid-cols-[3fr_2fr] gap-6">
-        <section className="">
-          {user && (
-            <div className="flex flex-col gap-6">
-              <Card classes="flex justify-between align-center">
-                <Link to="/settings" className="text-slate-500">
-                  <i className="fa-regular fa-gear fa-lg"></i>
-                </Link>
-              </Card>
-            </div>
-          )}
-          <Footer />
-        </section>
-        <aside className="">
-          {error && (
-            <div className="pb-3">
-              <ErrorAlert error={error} />
-            </div>
-          )}
-        </aside>
-      </div>
-    </main>
+    <div className="admin">
+      <h1>Admin</h1>
+      <Link to="/admin/food">
+        <Button variant="primary">Food</Button>
+      </Link>
+      <Link to="/admin/food-options">
+        <Button variant="primary">Food Options</Button>
+      </Link>
+    </div>
   );
-};
+}
 
 export default Admin;
