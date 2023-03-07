43c0fd9198a250f54ed0299c352015407fdc27f8
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-06 21:58:23 -0500

    test

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import './Admin.module.css';
+import FoodOptions from './FoodOptions';
 
 //go to FoodOptions.jsx
 const Admin = () => {
   return (
     <div>
       <h2>Admin</h2>
-      <Link to="/admin/foodOptions">
+      <Link to="/admin/food-options">
         <Button>Food Options</Button>
       </Link>
     </div>
