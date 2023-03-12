9ab00efa61a480147950684d44d51c0ce5bb10d5
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-11 23:22:52 -0500

    TEST

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -5,17 +5,32 @@ import { Button } from 'react-bootstrap';
 import './Admin.module.css';
 import FoodOptions from './FoodOptions';
 
-//go to FoodOptions.jsx
+
+
 const Admin = () => {
+  //check if user is logged in
+  //if not, redirect to login page
+  //if so, display admin page
+
   return (
     <div>
-      <h2>Admin</h2>
-      <Link to="/admin/food-options">
-        <Button>Food Options</Button>
+      <h1>Admin Page</h1>
+      <Link to="/admin/foodOptions">
+        <Button variant="primary">Food Options</Button>
       </Link>
+      <Link to="/admin/foodOptions/add">
+        <Button variant="primary">Add Food Option</Button>
+      </Link>
+      <Link to="/admin/foodOptions/edit">
+        <Button variant="primary">Edit Food Option</Button>
+      </Link>
+      <Link to="/admin/foodOptions/delete">
+        <Button variant="primary">Delete Food Option</Button>
+      </Link>
+     
+
     </div>
   );
-}
+};
 
 export default Admin;
-
