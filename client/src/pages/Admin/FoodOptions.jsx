8aa8d346d6f0eac3a2886c33e796c9dac0e86413
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-03-12 21:08:02 -0400

    test

--- client/src/pages/Admin/FoodOptions.jsx
+++ client/src/pages/Admin/FoodOptions.jsx
@@ -1,82 +1,98 @@
-//request the food table from the database
-//and display it in a table
-//give user the option to add, edit, or delete food prices
-//and to add, edit, or delete food items
-
+//Frontend page for admin users to view and edit food prices
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Table } from "react-bootstrap";
 import axios from "axios";
-import "./Admin.module.css";
+import "./FoodOptions.module.css";
 
-//connect to the database and get the food table
-//display the food table in a table
-//give user the option to add, edit, or delete food prices
-//and to add, edit, or delete food items
+//check if user is a admin user
+//display food table
+//display edit food price form
 
-const FoodOptions = () => {
+function FoodOptions() {
+    const [loggedIn, setLoggedIn] = useState(false);
     const [food, setFood] = useState([]);
     
-    //get the food table from the database
-    //get foodOptions table from the database
-
     useEffect(() => {
-        axios.get("/api/foodOptions").then((res) => {
-            setFood(res.data);
+        axios.get('/check-login-status')
+        .then(response => {
+            setLoggedIn(response.data.loggedIn);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+    
+    useEffect(() => {
+        axios.get('/food')
+        .then(response => {
+            setFood(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+    
+    if (!loggedIn) {
+        return (
+        <div>
+            <h1>Admin Login</h1>
+            <Link to="/login">
+            <Button variant="primary">Login</Button>
+            </Link>
+        </div>
+        );
+    }
+
+    //change food price
+
+    const changeFoodPrice = (event) => {
+        event.preventDefault();
+        const id = event.target.id.value;
+        const price = event.target.price.value;
+        axios.put(`/food/${id}`, { price })
+        .then(response => {
+            setFood(response.data);
+        })
+        .catch(error => {
+            console.log(error);
         });
     }
-    , []);
 
-    //delete a food item from the database
-
-    const deleteFood = (id) => {
-        axios.delete(`/api/foodOptions/${id}`).then((res) => {
-            setFood(food.filter((food) => food.id !== id));
-        });
-    }
-
-    //display the food table in a table
-    //give user the option to add, edit, or delete food prices
-
+    //display edit food price form
+    //display food table
+    
     return (
         <div>
             <h1>Food Options</h1>
+            <form onSubmit={changeFoodPrice}>
+                <label htmlFor="id">Food ID</label>
+                <input type="text" name="id" />
+                <label htmlFor="price">New Price</label>
+                <input type="text" name="price" />
+                <input type="submit" value="Change Price" />
+            </form>
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th>Food Item</th>
+                        <th>ID</th>
+                        <th>Name</th>
                         <th>Price</th>
-                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {food.map((food) => (
                         <tr key={food.id}>
-                            <td>{food.foodItem}</td>
+                            <td>{food.id}</td>
+                            <td>{food.name}</td>
                             <td>{food.price}</td>
-                            <td>
-                                <Link to={`/admin/foodOptions/edit/${food.id}`}>
-                                    <Button variant="primary">Edit</Button>
-                                </Link>
-                                <Button
-
-                                    variant="danger"
-                                    onClick={() => deleteFood(food.id)}
-                                >
-                                    Delete
-                                </Button>
-                            </td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
-            <Link to="/admin/foodOptions/add">
-                <Button variant="primary">Add Food Item</Button>
-            </Link>
         </div>
-
     );
-};
+}
 
 export default FoodOptions;
 
