0c29a09d61c8b54b2c15bda4e2c425eac7ee486b
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-03-17 15:50:43 -0400

    Setup to add additional styiling to navbar

--- client/src/components/Navbar/NotSignedIn/NotSignedIn.jsx
+++ client/src/components/Navbar/NotSignedIn/NotSignedIn.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './NotSignedIn.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NotSignedIn.module.css";
 const NotSignedIn = () => {
   return (
     <ul className={styles.nav}>
       <li className="px-3 py-2">
-        <Link to="/" className={styles.navLink}>
+        <Link to="/" className={`${styles.navLink}`}>
           Home
         </Link>
       </li>
       <li className="px-3 py-2">
-        <Link to="/login" className={styles.navLink}>
+        <Link to="/login" className={`${styles.navLink}`}>
           Login
         </Link>
       </li>
       <li className="px-3 py-2">
-        <Link to="/signup" className={styles.navLink}>
+        <Link to="/signup" className={`${styles.navLink}`}>
           Signup
         </Link>
       </li>
