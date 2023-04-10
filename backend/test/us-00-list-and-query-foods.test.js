497f005fbfc4d97986cc05c8ffc167d67c78e5d8
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-04-10 19:01:43 -0400

    test

--- backend/test/us-00-list-and-query-foods.test.js
+++ backend/test/us-00-list-and-query-foods.test.js
@@ -1,23 +1,26 @@
 const { expect } = require("chai");
 const request = require("supertest");
+const mongoose = require("mongoose");
 
 const app = require("../src/app");
-const knex = require("../src/db/connection");
+const Food = require("../src/models/Food");
 
 describe("00 - List and Query Food From Categories", () => {
-  beforeAll(() => {
-    return knex.migrate
-      .forceFreeMigrationsLock()
-      .then(() => knex.migrate.rollback(null, true))
-      .then(() => knex.migrate.latest());
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/your_db_name", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
   });
 
-  beforeEach(() => {
-    return knex.seed.run();
+  beforeEach(async () => {
+    await Food.deleteMany({});
+    // Insert test data if needed
   });
 
   afterAll(async () => {
-    return await knex.migrate.rollback(null, true).then(() => knex.destroy());
+    await mongoose.disconnect();
   });
 
   describe("Only allow GET request on /foods route", () => {
@@ -25,40 +28,40 @@ describe("00 - List and Query Food From Categories", () => {
       const response = await request(app)
         .post("/foods")
         .set("Accept", "application/json");
-
+  
       expect(response.status).to.equal(405);
       expect(response.body.error).to.equal("POST not allowed for /foods");
     });
-
+  
     test("Should return 405 status on PUT request", async () => {
       const response = await request(app)
         .put("/foods")
         .set("Accept", "application/json");
-
+  
       expect(response.status).to.equal(405);
       expect(response.body.error).to.equal("PUT not allowed for /foods");
     });
-
+  
     test("Should return 405 status on DELETE request", async () => {
       const response = await request(app)
         .delete("/foods")
         .set("Accept", "application/json");
-
+  
       expect(response.status).to.equal(405);
       expect(response.body.error).to.equal("DELETE not allowed for /foods");
     });
   });
-
+  
   describe("GET /foods", () => {
     test("Should return 200 for foods", async () => {
       const response = await request(app)
         .get("/foods")
         .set("Accept", "application/json");
-
+  
       expect(response.status).to.equal(200);
       expect(response.body.error).to.be.undefined;
     });
-
+  
     test("Should return a list of foods", async () => {
       const response = await request(app)
         .get("/foods")
@@ -66,7 +69,7 @@ describe("00 - List and Query Food From Categories", () => {
       const length = response.body.data.length;
       expect(length > 0).to.be.true;
     });
-
+  
     test("Should return a list of foods with proper format", async () => {
       const response = await request(app)
         .get("/foods")
@@ -80,7 +83,7 @@ describe("00 - List and Query Food From Categories", () => {
       expect(firstFood.available).to.equal(true);
     });
   });
-
+  
   describe("GET /foods", () => {
     test("Should return 404 if category is not found", async () => {
       const response = await request(app)
@@ -91,21 +94,21 @@ describe("00 - List and Query Food From Categories", () => {
         "Category: categoryThatDoesntExist does not exist"
       );
     });
-
+  
     test("Should return status of 200 from category", async () => {
       const response = await request(app)
         .get("/foods?category=diet_dishes")
         .set("Accept", "application/json");
-
+  
       expect(response.status).to.equal(200);
       expect(response.body.error).to.be.undefined;
     });
-
+  
     test("Should return an array of food from category", async () => {
       const response = await request(app)
         .get("/foods?category=diet_dishes")
         .set("Accept", "application/json");
-
+  
       const foods = response.body.data;
       const validFoodCategories = foods.filter(
         (food) => food.category === "diet_dishes"
