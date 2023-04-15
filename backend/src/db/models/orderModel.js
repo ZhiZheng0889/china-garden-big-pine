bbbf573abe1c3947aa059f5b136127f26a754e9c
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-04-14 20:26:05 -0400

    test

--- backend/src/db/models/orderModel.js
+++ backend/src/db/models/orderModel.js
@@ -1,23 +1,45 @@
+//Your order schema seems to have a small issue with the nested structure of the cart field.
+//  The cart should contain objects with properties such as food, specialRequest, quantity, etc. 
+//  To fix this issue, you should define the structure of the cart items as a separate schema and then use that schema 
+//  in the cart array. Here's the corrected schema:
+
 const mongoose = require("mongoose");
+
+const CartItemSchema = new mongoose.Schema({
+  food: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Food",
+  },
+  specialRequest: {
+    type: String,
+    default: "",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  selectedFoodOption: {
+    type: Number,
+    required: false,
+  },
+  selectedFoodSize: {
+    type: Number,
+    required: false,
+  },
+});
+
 const OrderSchema = new mongoose.Schema(
   {
     phoneNumber: { type: String, required: true },
     email: { type: String },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     isComplete: { type: Boolean, default: false },
-    cart: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "food",
-        specialRequest: { type: String, default: "" },
-        quantity: { type: Number, required: true },
-        selectedFoodOption: { type: Number, required: false },
-        selectedFoodSize: { type: Number, required: false },
-      },
-    ],
+    cart: [CartItemSchema],
   },
   { timestamps: true }
 );
 
 const Order = mongoose.model("Order", OrderSchema);
 module.exports = Order;
+
+
--- backend/test/us-03-rest-orders.test.js
+++ backend/test/us-03-rest-orders.test.js
@@ -1,25 +1,57 @@
 const { expect } = require("chai");
 const request = require("supertest");
 const app = require("../src/app");
-/*
-Create rest api for orders to be able to list, 
-read one order, 
-update order, 
-create order, 
-delete order
-*/
 
-describe("01 - List, Read, Create, update, and Delete orders", () => {
+describe("01 - List, Read, Create, Update, and Delete orders", () => {
   describe("List orders by user", () => {
-    test("Should return 404 error if user _id is not found", () => {});
+    test("Should return 404 error if user _id is not found", async () => {
+      const userId = "nonExistentUserId";
+      const response = await request(app)
+        .get(`/orders/user/${userId}`)
+        .set("Accept", "application/json");
 
-    test("Should return a list of orders", () => {});
+      expect(response.status).to.equal(404);
+      expect(response.body.error).to.contain("User not found");
+    });
+
+    test("Should return a list of orders", async () => {
+      const userId = "validUserId";
+      // TODO: Replace "validUserId" with an actual user ID from your database.
+
+      const response = await request(app)
+        .get(`/orders/user/${userId}`)
+        .set("Accept", "application/json");
+
+      expect(response.status).to.equal(200);
+      expect(response.body.data).to.be.an("array");
+    });
   });
 
   describe("Read order from order _id ", () => {
-    test("Should return 404 error if order _id is not found", () => {});
+    test("Should return 404 error if order _id is not found", async () => {
+      const orderId = "nonExistentOrderId";
+      const response = await request(app)
+        .get(`/orders/${orderId}`)
+        .set("Accept", "application/json");
 
-    test("Should return an order from order_id without phone number, email, or user id", () => {});
+      expect(response.status).to.equal(404);
+      expect(response.body.error).to.contain("Order not found");
+    });
+
+    test("Should return an order from order_id without phone number, email, or user id", async () => {
+      const orderId = "validOrderId";
+      // TODO: Replace "validOrderId" with an actual order ID from your database.
+
+      const response = await request(app)
+        .get(`/orders/${orderId}`)
+        .set("Accept", "application/json");
+
+      expect(response.status).to.equal(200);
+      expect(response.body.data).to.be.an("object");
+      expect(response.body.data.phoneNumber).to.be.undefined;
+      expect(response.body.data.email).to.be.undefined;
+      expect(response.body.data.user).to.be.undefined;
+    });
   });
 
   describe("Create order", () => {
@@ -29,12 +61,13 @@ describe("01 - List, Read, Create, update, and Delete orders", () => {
         .post("/orders")
         .set("Accept", "application/json")
         .send({ data });
+
       expect(response.status).to.equal(400);
       expect(response.body.error).to.contain("phoneNumber");
     });
 
     test("Should return 400 error if phone number is not a valid phone number", async () => {
-      const data = {};
+      const data = { phoneNumber: "invalidPhoneNumber" };
       const response = await request(app)
         .post("/orders")
         .set("Accept", "application/json")
@@ -44,45 +77,290 @@ describe("01 - List, Read, Create, update, and Delete orders", () => {
       expect(response.body.error).to.contain("phoneNumber");
     });
 
-    test("Should return 400 error if email is not null and not a valid email", async () => {});
+  // TODO: Add test cases for update and delete order scenarios
 
-    test("Should create order without a user _id", async () => {});
+  test("Should return 400 error if email is not null and not a valid email", async () => {
+    const data = { phoneNumber: "555-555-5555", email: "invalidEmail" };
+    const response = await request(app)
+      .post("/orders")
+      .set("Accept", "application/json")
+      .send({ data });
+  
+    expect(response.status).to.equal(400);
+    expect(response.body.error).to.contain("email");
+  });
+  
 
-    test("Should create order without a email", async () => {});
+  test("Should create order without a user _id", async () => {
+    const data = { phoneNumber: "555-555-5555", email: "test@example.com" };
+    const response = await request(app)
+      .post("/orders")
+      .set("Accept", "application/json")
+      .send({ data });
+  
+    expect(response.status).to.equal(201);
+    expect(response.body.data.phoneNumber).to.equal(data.phoneNumber);
+    expect(response.body.data.email).to.equal(data.email);
+    expect(response.body.data.user).to.be.undefined;
+  });
+  
+
+  test("Should create order without an email", async () => {
+    const data = { phoneNumber: "555-555-5555" };
+    const response = await request(app)
+      .post("/orders")
+      .set("Accept", "application/json")
+      .send({ data });
+  
+    expect(response.status).to.equal(201);
+    expect(response.body.data.phoneNumber).to.equal(data.phoneNumber);
+    expect(response.body.data.email).to.be.undefined;
+  });
+  
     describe("Cart", () => {
-      test("Should return 400 error if cart does not exist", async () => {});
+      test("Should return 400 error if cart does not exist", async () => {
+        const data = { phoneNumber: "555-555-5555" };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("cart");
+      });
+      
+      test("Should return 400 error if cart is not an array", async () => {
+        const data = { phoneNumber: "555-555-5555", cart: "notAnArray" };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("cart");
+      });
+      
+      test("Should return 400 error if cart item is not an object", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: ["notAnObject"],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("cart item");
+      });
+      
+      test("Should return 400 error if cart item does not have a food id", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("food id");
+      });
+      
 
-      test("Should return 400 error if cart is not an array", async () => {});
+      test("Should return 400 error if cart item food _id is not null", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: null, quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("food _id");
+      });
+      
+      test("Should return 400 error if cart item food _id is not a number", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: "notANumber", quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("food _id");
+      });
+      
+      test("Should return 400 error if cart item does not have a quantity", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("quantity");
+      });
+      
+      test("Should return 400 error if cart item quantity is not a number", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: "notANumber" }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("quantity");
+      });
+      
+      test("Should return 400 error if cart item specialRequest is not a string", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1, specialRequest: 123 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("specialRequest");
+      });
+      
+      test("Should return 400 error if cart item specialRequest is not at most a certain length", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [
+            {
+              food_id: 1,
+              quantity: 1,
+              specialRequest: "This special request is too long and should result in an error.",
+            },
+          ],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("specialRequest");
+      });      
 
-      test("Should return 400 error if cart item is not an object", async () => {});
+      test("Should return 400 error if cart item selectedFoodOption is not a number", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1, selectedFoodOption: "notANumber" }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("selectedFoodOption");
+      });
+      
+      test("Should return 400 error if cart item selectedFoodSize is not a number", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1, selectedFoodSize: "notANumber" }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("selectedFoodSize");
+      });
+      
+      test("Should return 400 error if cart item does not have a valid property", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1, invalidProperty: "invalidValue" }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("invalidProperty");
+      });
+      
+      test("Should return 400 error if cart item does not have all required properties", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.contain("required");
+      });
+      
 
-      test("Should return 400 error if cart item does not have a food id", async () => {});
-
-      test("Should return 400 error if cart item food _id is not null", async () => {});
-
-      test("Should return 400 error if cart item food _id is not a number", async () => {});
-
-      test("Should return 400 error if cart item does not have a quantity", async () => {});
-
-      test("Should return 400 error if cart item quantity is not a number", async () => {});
-
-      test("Should return 400 error if cart item specialRequest is not a string", async () => {});
-
-      test("Should return 400 error if cart item specialRequest is not at most a certain length", async () => {});
-
-      test("Should return 400 error if cart item selectedFoodOption is not a number", async () => {});
-
-      test("Should return 400 error if cart item selectedFoodSize is not a number", async () => {});
-
-      test("Should return 400 error if cart item does not have a valid property", async () => {});
-
-      test("Should return 400 error if cart item does not have all required properties", async () => {});
-
-      test("Should create order if specialRequest does not exist", async () => {});
-
-      test("Should create order if selectedFoodOption does not exist", async () => {});
-
-      test("Should create order if selectedFoodSize does not exist", async () => {});
+      test("Should create order if specialRequest does not exist", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(201);
+        expect(response.body.order).to.be.an("object");
+        expect(response.body.order.cart[0].specialRequest).to.equal("");
+      });
+      
+      test("Should create order if selectedFoodOption does not exist", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(201);
+        expect(response.body.order).to.be.an("object");
+        expect(response.body.order.cart[0].selectedFoodOption).to.be.undefined;
+      });
+      
+      test("Should create order if selectedFoodSize does not exist", async () => {
+        const data = {
+          phoneNumber: "555-555-5555",
+          cart: [{ food_id: 1, quantity: 1 }],
+        };
+        const response = await request(app)
+          .post("/orders")
+          .set("Accept", "application/json")
+          .send({ data });
+      
+        expect(response.status).to.equal(201);
+        expect(response.body.order).to.be.an("object");
+        expect(response.body.order.cart[0].selectedFoodSize).to.be.undefined;
+      });
+      
     });
   });
 
