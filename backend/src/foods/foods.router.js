c5a712798d52a0a4c74a0c954abe351d75478e9c
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-04-18 03:33:30 -0400

    test

--- backend/src/foods/foods.router.js
+++ backend/src/foods/foods.router.js
@@ -4,18 +4,4 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 
-<<<<<<< HEAD
-=======
-// app.get('/foods', async (req, res) => {
-//     const category = req.query.category;
-//     const foods = await getFoodsByCategory(category);
-
-//     if (category && !foods.length) {
-//       res.status(404).json({ error: `Category: ${category} does not exist` });
-//     } else {
-//       res.status(200).json({ data: foods });
-//     }
-//   });
-
->>>>>>> 015e24878581b54dd13cfe5e29570a30a0af7b46
 module.exports = router;
