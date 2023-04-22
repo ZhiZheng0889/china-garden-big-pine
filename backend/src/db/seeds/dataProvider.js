98330a902deeb52d5d5bf4880d69f8b9229fa322
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-04-22 14:07:45 -0400

    Remove old code commented out

--- backend/src/db/seeds/dataProvider.js
+++ backend/src/db/seeds/dataProvider.js
@@ -61,63 +61,6 @@ if (process.argv[4]) {
   }
 }
 
-// if (process.argv[2] === "--import" && process.argv[3] === "--all") {
-//   seed();
-// } else if (process.argv[2] === "--import" && process.argv[3] !== undefined) {
-//   seed(process.argv[3]);
-// } else if (process.argv[2] === "--delete" && process.argv[3] === "--all") {
-//   destroy();
-// } else if (process.argv[2] === "--delete" && process.argv[3] !== undefined) {
-//   destroy(process.argv[3]);
-// }
-
-// async function seed(type = "") {
-//   if (type === "--users" || !type) {
-
-//     await Seeder.inject(`${__dirname}/users.json`, User);
-//   }
-//   if (type === "--foods" || !type) {
-//     await Seeder.inject(`${__dirname}/foods.json`, Food);
-//   }
-//   if (type === "--orders" || !type) {
-//     await Seeder.inject(`${__dirname}/orders.json`, Order);
-//   }
-// }
-
-// async function seedTest(type = "") {
-//   if (type === "--users" || !type) {
-//     await Seeder.inject(`${__dirname}/users.json`, User);
-//   }
-//   if (type === "--foods" || !type) {
-//     await Seeder.inject(`${__dirname}/foods.json`, Food);
-//   }
-//   if (type === "--orders" || !type) {
-//     await Seeder.inject(`${__dirname}/orders.json`, Order);
-//   }
-// }
-
-// async function destroy(type = "") {
-//   if (type === "--users" || !type) {
-//     await Seeder.delete(User);
-//   }
-//   if (type === "--foods" || !type) {
-//     await Seeder.delete(Food);
-//   }
-//   if (type === "--orders" || !type) {
-//     await Seeder.delete(Order);
-//   }
-// }
-
-// if (process.argv[2] === "--import" && process.argv[3] === "--all") {
-//   seed();
-// } else if (process.argv[2] === "--import" && process.argv[3] !== undefined) {
-//   seed(process.argv[3]);
-// } else if (process.argv[2] === "--delete" && process.argv[3] === "--all") {
-//   destroy();
-// } else if (process.argv[2] === "--delete" && process.argv[3] !== undefined) {
-//   destroy(process.argv[3]);
-// }
-
 module.exports = {
   seedAll,
   reapAll,
