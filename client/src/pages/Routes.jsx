fd87a06b7892f06e81e2b915e7ce14f4a1037722
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-04-25 23:50:37 -0400

    Add settings

--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -11,6 +11,7 @@ import Profile from "./Profile/Profile";
 import Order from "./Order/Order";
 import Orders from "./Orders/Orders";
 import FoodOptions from "./Admin/FoodOptions";
+import Settings from "./Settings/Settings";
 
 const PageRoutes = ({
   cart,
@@ -58,6 +59,7 @@ const PageRoutes = ({
       </Route>
       <Route path="/login" element={<Login setUser={setUser} />} />
       <Route path="/signup" element={<Signup setUser={setUser} />} />
+      <Route path="/settings" element={<Settings />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
