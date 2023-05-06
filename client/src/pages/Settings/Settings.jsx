03ec3e66c8cb4497d0006f45a934429ff4f97dc0
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-05-06 13:53:20 -0400

    Add settings page

--- client/src/pages/Settings/Settings.jsx
+++ client/src/pages/Settings/Settings.jsx
@@ -10,7 +10,23 @@ const Settings = ({ user, setUser }) => {
   if (isObjectEmpty(user)) {
     navigate("/");
   }
-  const [isEditingFirstName, setIsEditingFirstName] = useState(false);
+  const [isEditing, setIsEditing] = useState({
+    firstName: false,
+  });
+  const editProfile = (event) => {
+    event.preventDefault();
+    const key = event.target.getAttribute("data-setting");
+    console.log("key: ", key);
+    setIsEditing((curr) => {
+      return { ...curr, [key]: true };
+    });
+  };
+  const changeProfile = ({ target: { value, id } }) => {
+    setUser((curr) => {
+      return { ...curr, [id]: value };
+    });
+  };
+  const cancelChanges = () => {};
   return (
     <>
       <main className="min-h-screen bg-slate-100 py-6">
@@ -20,20 +36,98 @@ const Settings = ({ user, setUser }) => {
               <h3 className="font-semibold text-xl">Settings</h3>
             </header>
             <form className="p-3 flex flex-col gap-3">
-              {isEditingFirstName ? (
-                <div className="flex">
-                  <p></p>
-                </div>
-              ) : (
-                <div className="flex flex-col gap-1">
-                  <label htmlFor="firstName">First Name</label>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="firstName">First Name</label>
+                {isEditing.firstName ? (
                   <input
                     type="text"
                     id="firstName"
                     className="p-2 border rounded focus:outline outline-2 outline-offset-2 outline-red-600"
                     value={user.firstName}
                     placeholder="First Name..."
+                    onChange={changeProfile}
                   />
+                ) : (
+                  <div className="flex justify-between items-center">
+                    <p className="text-neutral-500">{user.firstName}</p>
+                    <button
+                      className="w-10 h-10 ease-out duration-200 flex justify-center items-center rounded hover:bg-neutral-100 active:bg-neutral-200"
+                      data-setting="firstName"
+                      onClick={editProfile}
+                    >
+                      <i
+                        className="fa-solid fa-pen-to-square"
+                        data-setting="firstName"
+                      ></i>
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="email">Email</label>
+                {isEditing.email ? (
+                  <input
+                    type="email"
+                    id="email"
+                    className="p-2 border rounded focus:outline outline-2 outline-offset-2 outline-red-600"
+                    value={user.email}
+                    placeholder="Email..."
+                    onChange={changeProfile}
+                  />
+                ) : (
+                  <div className="flex justify-between items-center">
+                    <p className="text-neutral-500">{user.email}</p>
+                    <button
+                      className="w-10 h-10 ease-out duration-200 flex justify-center items-center rounded hover:bg-neutral-100 active:bg-neutral-200"
+                      data-setting="email"
+                      onClick={editProfile}
+                    >
+                      <i
+                        className="fa-solid fa-pen-to-square"
+                        data-setting="email"
+                      ></i>
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                {isEditing.phoneNumber ? (
+                  <input
+                    type="tel"
+                    id="phoneNumber"
+                    className="p-2 border rounded focus:outline outline-2 outline-offset-2 outline-red-600"
+                    value={user.phoneNumber}
+                    placeholder="Phone Number..."
+                    onChange={changeProfile}
+                  />
+                ) : (
+                  <div className="flex justify-between items-center">
+                    <p className="text-neutral-500">{user.phoneNumber}</p>
+                    <button
+                      className="w-10 h-10 ease-out duration-200 flex justify-center items-center rounded hover:bg-neutral-100 active:bg-neutral-200"
+                      data-setting="phoneNumber"
+                      onClick={editProfile}
+                    >
+                      <i
+                        className="fa-solid fa-pen-to-square"
+                        data-setting="phoneNumber"
+                      ></i>
+                    </button>
+                  </div>
+                )}
+              </div>
+              {Object.values(isEditing).some((_) => _ === true) && (
+                <div className="flex justify-center items-center gap-3">
+                  <button className="w-32 px-3 py-2 border border-red-700 rounded hover:bg-red-700 hover:text-white active:text-white active:bg-red-800 ease-out duration-200 text-red-700">
+                    Cancel
+                  </button>
+                  <button
+                    className="w-32 px-3 py-2 bg-red-600 rounded hover:bg-red-700 active:bg-red-800 ease-out duration-200 text-white"
+                    onClick={cancelChanges}
+                  >
+                    Save Changes
+                  </button>
                 </div>
               )}
             </form>
