3763e4b2cf8c30480c6383c604c2e61ebb3f4362
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-05-17 17:12:17 -0400

    Trying to fix login token bug.

--- backend/src/user/user.router.js
+++ backend/src/user/user.router.js
@@ -5,8 +5,13 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 router.route("/login/google", controller.loginWithGoogle);
 router
   .route("/login/token")
-  .post(controller.loginWithToken)
+  .post((req, res, next) => {
+    console.log('Cookies: ', req.cookies);
+    console.log('Body: ', req.body);
+    next();
+  }, asyncErrorBoundary(controller.loginWithToken))
   .all(methodNotAllowed);
+  
 router.route("/login").post(controller.login).all(methodNotAllowed);
 router.route("/").post(controller.register).all(methodNotAllowed);
 
