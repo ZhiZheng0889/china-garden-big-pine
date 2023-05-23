a5c4211e113ae5229f4a148cebdf3ecdef9e8ab3
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-05-23 19:12:08 -0400

    test

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,78 +1,40 @@
-//admin page for admin users
-//check if user is logged in
-//if user is not logged in, redirect to login page
-//if user is logged in, display admin page
-//display admin page
-
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Button, Table } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import "./Admin.module.css";
 
-//connect to the database and check if user is logged in
-//if user is not logged in, redirect to login page
-//if user is logged in, display admin page
-
 function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
-    axios.get('/check-login-status')
-      .then(response => {
-        setLoggedIn(response.data.loggedIn);
+    axios
+      .get("/check-user")
+      .then((response) => {
+        setIsLoggedIn(response.data.loggedIn);
+        setIsAdmin(response.data.isAdmin);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  if (!loggedIn) {
-    return (
-      <div>
-        <h1>Admin Login</h1>
-        <Link to="/login">
-          <Button variant="primary">Login</Button>
-        </Link>
-      </div>
-    );
+  if (!isLoggedIn) {
+    history.push("/login");
   }
 
-  //check user token
-  //if user token is not valid, redirect to login page
-  //if user token is valid, display admin page
-
-  useEffect(() => {
-    axios.get('/check-user-token')
-      .then(response => {
-        if (!response.data.valid) {
-          setLoggedIn(false);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
-  if (!loggedIn) {
-    return (
-      <div>
-        <h1>Admin Login</h1>
-        <Link to="/login">
-          <Button variant="primary">Login</Button>
-        </Link>
-      </div>
-    );
-
+  if (!isAdmin && isLoggedIn) {
+    history.push("/"); // assuming "/" is your homepage for non-admin users
   }
-
-  //display admin page
 
   return (
-    <div>
-      <h1>Admin Page</h1>
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold mb-4">Admin Page</h1>
       <Link to="/admin/foodOptions">
-        <Button variant="primary">Food Options</Button>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Food Options
+        </button>
       </Link>
     </div>
   );
