be1600bf897fa578470cdc8e9d30e6b4ea7e7b4d
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-06-03 13:09:47 -0400

    test

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 const Searchbar = ({ search, setSearch }) => {
-  /**
-   * when search is submitted update the value search in the query object to the current value of the input text
-   *
-   */
-
   const [keyword, setKeyword] = useState("");
   useEffect(() => {
     if (!search) {
@@ -20,18 +15,29 @@ const Searchbar = ({ search, setSearch }) => {
     setKeyword(target.value);
   };
 
+  const clearInput = () => {
+    setKeyword('');
+  };
+
   return (
     <form className="relative" onSubmit={submitHandler}>
       <button
-        className="w-10 h-10 hover:bg-neutral-100 active:bg-neutral-200 ease-out duration-200 rounded absolute top-2/4 right-[2%] -translate-y-2/4 focus:outline outline-2 outline-offset-2 outline-red-600  "
+        className="w-10 h-10 hover:bg-neutral-100 active:bg-neutral-200 ease-out duration-200 rounded absolute top-2/4 right-[4%] -translate-y-2/4 focus:outline outline-2 outline-offset-2 outline-red-600  "
         type="submit"
       >
         <i className="fa-solid fa-magnifying-glass fa-lg"></i>
       </button>
+      <button
+        onClick={clearInput}
+        className="w-10 h-10 hover:bg-neutral-100 active:bg-neutral-200 ease-out duration-200 rounded absolute top-2/4 right-[2%] -translate-y-2/4 focus:outline outline-2 outline-offset-2 outline-red-600  "
+        type="button"
+      >
+        <i className="fa-solid fa-times fa-lg"></i>
+      </button>
       <input
         type="text"
         placeholder="Search"
-        className="w-full py-3 pl-4 pr-[2.7rem] border rounded focus:outline outline-2 outline-offset-2 outline-red-600"
+        className="w-full py-3 pl-4 pr-[4.7rem] border rounded focus:outline outline-2 outline-offset-2 outline-red-600"
         value={keyword}
         onChange={onChange}
       />
