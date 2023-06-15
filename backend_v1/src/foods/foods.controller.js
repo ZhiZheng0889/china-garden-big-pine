2cb1fb446c066e0272bcebb3c7a047d13ed5b227
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-14 22:15:57 -0400

    Update food controller with pagination

--- backend_v1/src/foods/foods.controller.js
+++ backend_v1/src/foods/foods.controller.js
@@ -1,21 +1,35 @@
 const service = require("./foods.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const { PAGINATION } = process.env;
 
 async function getAllFoods(req, res, next) {
   const { category = "", page = 1 } = req.query;
-  console.log("category: ", category, page);
-  const results = await service.getAll(category, parseInt(page));
+  const results = category
+    ? await service.getByCategory(
+        category,
+        parseInt(page),
+        parseInt(PAGINATION)
+      )
+    : await service.getAll(category, parseInt(page), parseInt(PAGINATION));
   if (category && !results.length) {
     return next({
       status: 404,
-      message: `Category: ${category} does not exist`,
+      message: `Category: "${category}" does not exist`,
     });
   }
-  res.status(200).json({ results, page });
+  res.status(200).json({ results, page, pagination: PAGINATION });
 }
 
 async function getSearchedFoods(req, res, next) {
-  const { search = "" } = req.query;
+  const { search = "", page = 1 } = req.query;
+  const results = await service.getBySearch(search, page, PAGINATION);
+  if (search && !results.length) {
+    return next({
+      status: 404,
+      message: `Search: "${search}" cannot be found`,
+    });
+  }
+  res.status(200).json({ results, page, pagination: PAGINATION });
 }
 
 module.exports = {
--- backend_v1/src/foods/foods.service.js
+++ backend_v1/src/foods/foods.service.js
@@ -1,1 +1,37 @@
 const Food = require("../db/models/foodModel");
+
+function getAll(page, pagination) {
+  return Food.find()
+    .skip((parseInt(page) - 1) * parseInt(pagination))
+    .limit(parseInt(pagination));
+}
+
+function getBySearch(text, page, pagination) {
+  const includeUnderScoreText = text.trim().split(" ").join("_");
+  return Food.find({
+    $or: [
+      { name: { $regex: text, $options: "i" } },
+      { category: { $regex: text, $options: "i" } },
+      { name: { $regex: includeUnderScoreText, $options: "i" } },
+    ],
+  })
+    .skip((parseInt(page) - 1) * parseInt(pagination))
+    .limit(parseInt(pagination));
+}
+
+function getByCategory(category, page, pagination) {
+  const includeUnderScoreText = category
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .join("_");
+  return Food.find({ category: includeUnderScoreText })
+    .skip((parseInt(page) - 1) * parseInt(pagination))
+    .limit(parseInt(pagination));
+}
+
+module.exports = {
+  getAll,
+  getBySearch,
+  getByCategory,
+};
