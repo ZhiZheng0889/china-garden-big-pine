e2847de1331d62893eed5dfecebc1cf85147d894
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-06-19 14:24:15 -0400

    Update is open

--- client/src/utils/isOpen.js
+++ client/src/utils/isOpen.js
@@ -1,9 +1,23 @@
 import dayjs from "dayjs";
 
+function parseTime(time) {
+  const [hours, minutes] = time.split(":");
+  return new Date(0, 0, 0, hours, minutes);
+}
+
+function isTimeBetween(startTime, endTime, currentTime) {
+  const start = parseTime(startTime);
+  const end = parseTime(endTime);
+  const current = parseTime(currentTime);
+
+  return start <= current && current <= end;
+}
+
 export const isOpen = (hours) => {
-  console.log(hours);
-  const currentTime = new Date().toLocaleString("en-US", {
-    timeZone: "America/New_York",
-  });
-  console.log(currentTime);
+  const currentTime = dayjs(
+    new Date().toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    })
+  ).format("HH:mm");
+  return isTimeBetween(hours.open, hours.close, currentTime);
 };
