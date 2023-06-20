506a4093e8d4b364b19b0bf172265c03984a8c5a
ZhiZheng0889 <zhizheng33782@gmail.com>
2023-06-20 00:58:32 -0400

    test

--- backend/src/db/models/foodModel.js
+++ backend/src/db/models/foodModel.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
-const AutoIncrementFactory = require('mongoose-sequence');
-
-const connection = mongoose.createConnection("mongodb://localhost/test");
-const AutoIncrement = AutoIncrementFactory(connection);
 
 const FoodSchema = new mongoose.Schema(
   {
-    food_id: { type: Number },
     name: { type: String, required: [true, "A food requires a name"] },
     basePrice: {
       type: Number,
@@ -42,8 +37,6 @@ const FoodSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-FoodSchema.plugin(AutoIncrement, { inc_field: 'food_id' });
-
-const Food = connection.model('Food', FoodSchema);
+const Food = mongoose.model("Food", FoodSchema);
 
 module.exports = Food;
